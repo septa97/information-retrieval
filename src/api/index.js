834f9b1294d4109fe0ac8b4e5fa0610fbbab958a
septa97 <jancharles.adona@gmail.com>
2017-04-17 16:57:55 +0800

    Edited index.js

--- src/api/index.js
+++ src/api/index.js
@@ -2,14 +2,12 @@ import { version } from '../../package.json';
 import { Router } from 'express';
 import facets from './facets';
 import reviews from './reviews';
-import search_engine from './../controllers/search_engine'
 
 export default ({ config, db }) => {
 	let api = Router();
 
 	api.use('/facets', facets({ config, db }));
 	api.use('/reviews', reviews({ config, db }));
-	api.get('/search', search_engine.search1);
 
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
